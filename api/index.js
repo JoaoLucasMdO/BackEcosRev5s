3f46ecb757de6cc4340ad8e694ed06b8a7a2f410
João Lucas Melo de Oliveira <joao.oliveira307@fatec.sp.gov.br>
2025-06-25 12:47:16 -0300

    Atualização cors

--- api/index.js
+++ api/index.js
@@ -11,8 +11,7 @@ const { PORT } = process.env;
 
 // ✅ MIDDLEWARE CORS — deve vir antes das rotas
 app.use(cors({
-  origin: ["http://18.215.24.30", 
-    "http://ecosrev.duckdns.org"], 
+  origin: '*', 
   credentials: true,
 }));
 
