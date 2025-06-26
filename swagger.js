e84c00e9b64d03459305711eb677c6bb458ee1db
João Lucas Melo de Oliveira <joao.oliveira307@fatec.sp.gov.br>
2025-06-26 00:54:01 -0300

    ATT

--- swagger.js
+++ swagger.js
@@ -8,7 +8,7 @@ const doc = {
         title: "Projeto EcosRev 🍀",
         description: "➡️Documentação gerada automaticamente pelo módulo <a href='https://github.com/davibaltar/swagger-autogen' target='_blank'>swagger-autogen</a>."
     },
-    host: '18.215.24.30',// LINK DO VERCEL VAI AQUI
+    host: 'ecosrev-api.duckdns.org',// LINK DO VERCEL VAI AQUI
     basePath: "",
     schemes: ['https'], //NO VERCEL DARA ERRO, TROCAR POR HTTPS AMEM
     consumes: ['application/json'],
